e9c50380de7886244de997e32651facec78b8d02
hungga <vuthehung17@gmail.com>
2016-06-05 15:00:16 +0700

    HUNG 2

---
+++ Techkid_Chess/Techkid_Chess/ChatBoxViewController.h
@@ -0,0 +1,14 @@
+//
+//  ChatBoxViewController.h
+//  Techkid_Chess
+//
+//  Created by Quang Dai on 5/29/16.
+//  Copyright © 2016 TechKid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatBoxViewController : UIViewController
+
+
+@end
